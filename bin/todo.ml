3d924d797adf08b15a83c05cc0c01d4862379b46
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-02-20 18:21:34 +1100

    add Inter font but not using it for now

--- bin/todo.ml
+++ bin/todo.ml
@@ -1,41 +1,58 @@
-(* Layout *)
+(* dodgy globals *)
+(* let font = Raylib.load_font_ex "bin/Inter-Regular.ttf" 32 (Raylib.ptr_of_int 0) 0 *)
+
+
+(* -- Layout *)
 
 type rect = (int * int * int * int)
 type colour = (int * int * int * int)
 type border = bool * colour
 
 type element =
-  Box of rect * border * colour
-  | Text of string * int            (* text and font size *)
+  Rect of rect * border * Raylib.Color.t
+  | Text of int * int * string * int            (* text and font size *)
 
 type layout =
-  VStack of element list list
-  | HStack of element list list
+  VStack of layout list
+  | HStack of layout list
+  | Box of element list
 
-let raylib_draw_rect _ _ = ()
 
+(* Components *)
+
+(* Button / Text input / Icon / ListBox *)
+
+let pos = Raylib.Vector2.create 10.0 10.0
 let draw el = match el with
-  | Box (r, _, c) -> raylib_draw_rect r c
-  | Text -> ()
+  | Rect ((x, y, w, h), _, c) -> Raylib.draw_rectangle x y w h c
+  | Text (x, y, text, font_size) -> Raylib.draw_text text x y font_size Raylib.Color.black
 
-let render (root: layout) =
+let rec render (root: layout) =
   match root with
   (* eventually have a responsive spacing algo here to change where rects are drawn *)
-  | VStack (rows) -> List.iter (List.iter draw) rows
-  | _ -> ()
+  | VStack (rows) -> List.iter render rows
+  | HStack (rows) -> List.iter render rows
+  | Box (els) -> List.iter draw els
 
-(* Main *)
+(* -- Main *)
 
 let grey = ( 100, 100, 100, 1)
 let white = ( 255, 255, 255, 1)
-let header = Box ((0, 0, 800, 100), (false, grey), grey)
-let body = Box ((0, 100, 800, 540), (false, grey), white)
 
-let x = VStack [[header]; [body]]
+(* let x = 
+  VStack [[header; Text (360, 15, "snap-todo", 15)]; [body]] *)
+
+let x =
+  VStack [
+    Box [Rect ((0, 0, 800, 50), (false, grey), Raylib.Color.darkgreen)] ;
+    Box [Rect ((0, 100, 800, 590), (false, grey), Raylib.Color.raywhite)]
+  ]
+
 
 let setup () =
   Raylib.init_window 800 640 "snap-todo";
-  Raylib.set_target_fps 60
+  Raylib.set_target_fps 60;
+  ()
 
 let rec loop () =
   match Raylib.window_should_close () with
@@ -44,8 +61,7 @@ let rec loop () =
       let open Raylib in
       begin_drawing ();
       clear_background Color.raywhite;
-      draw_text "Congrats! You created your first window!" 190 200 20
-        Color.lightgray;
+      render x;
       end_drawing ();
       loop ()
 
